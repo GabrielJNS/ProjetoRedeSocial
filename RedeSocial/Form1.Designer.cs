583f9c01439101e60884e17567a8d061eae3c781
lordumungus <alessandro.costa6@fatec.sp.gov.br>
2023-08-31 21:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ RedeSocial/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace RedeSocial
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.bt_logar = new System.Windows.Forms.Button();
+            this.bt_cadastrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(172, 198);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(223, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // bt_logar
+            // 
+            this.bt_logar.Location = new System.Drawing.Point(156, 251);
+            this.bt_logar.Name = "bt_logar";
+            this.bt_logar.Size = new System.Drawing.Size(257, 68);
+            this.bt_logar.TabIndex = 2;
+            this.bt_logar.Text = "Login";
+            this.bt_logar.UseVisualStyleBackColor = true;
+            // 
+            // bt_cadastrar
+            // 
+            this.bt_cadastrar.Location = new System.Drawing.Point(154, 347);
+            this.bt_cadastrar.Name = "bt_cadastrar";
+            this.bt_cadastrar.Size = new System.Drawing.Size(258, 71);
+            this.bt_cadastrar.TabIndex = 3;
+            this.bt_cadastrar.Text = "Cadastrar";
+            this.bt_cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(180, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 73);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LOGIN";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 470);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_cadastrar);
+            this.Controls.Add(this.bt_logar);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button bt_logar;
+        private System.Windows.Forms.Button bt_cadastrar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
