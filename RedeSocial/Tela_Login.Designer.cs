772ff5d5b2ed7c0affa81ce6c8042039327eaa34
Alessandro <133154637+lordumungus@users.noreply.github.com>
2023-09-14 15:37:31 -0300

    Adição do botão foto de perfil
    E renome das telas.

---
+++ RedeSocial/Tela_Login.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace RedeSocial
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.caixa_nome = new System.Windows.Forms.TextBox();
+            this.caixa_senha = new System.Windows.Forms.MaskedTextBox();
+            this.bt_logar = new System.Windows.Forms.Button();
+            this.bt_cadastrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_Vetor = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // caixa_nome
+            // 
+            this.caixa_nome.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.caixa_nome.Location = new System.Drawing.Point(183, 136);
+            this.caixa_nome.Name = "caixa_nome";
+            this.caixa_nome.Size = new System.Drawing.Size(258, 20);
+            this.caixa_nome.TabIndex = 0;
+            // 
+            // caixa_senha
+            // 
+            this.caixa_senha.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.caixa_senha.Location = new System.Drawing.Point(183, 179);
+            this.caixa_senha.Name = "caixa_senha";
+            this.caixa_senha.Size = new System.Drawing.Size(257, 20);
+            this.caixa_senha.TabIndex = 1;
+            this.caixa_senha.UseSystemPasswordChar = true;
+            // 
+            // bt_logar
+            // 
+            this.bt_logar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bt_logar.Location = new System.Drawing.Point(183, 213);
+            this.bt_logar.Name = "bt_logar";
+            this.bt_logar.Size = new System.Drawing.Size(257, 68);
+            this.bt_logar.TabIndex = 2;
+            this.bt_logar.Text = "Login";
+            this.bt_logar.UseVisualStyleBackColor = true;
+            this.bt_logar.Click += new System.EventHandler(this.bt_logar_Click);
+            // 
+            // bt_cadastrar
+            // 
+            this.bt_cadastrar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bt_cadastrar.Location = new System.Drawing.Point(182, 325);
+            this.bt_cadastrar.Name = "bt_cadastrar";
+            this.bt_cadastrar.Size = new System.Drawing.Size(258, 71);
+            this.bt_cadastrar.TabIndex = 3;
+            this.bt_cadastrar.Text = "Cadastrar";
+            this.bt_cadastrar.UseVisualStyleBackColor = true;
+            this.bt_cadastrar.Click += new System.EventHandler(this.Bt_cadastrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 73);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LOGIN";
+            // 
+            // label_Vetor
+            // 
+            this.label_Vetor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label_Vetor.AutoSize = true;
+            this.label_Vetor.Location = new System.Drawing.Point(568, 163);
+            this.label_Vetor.Name = "label_Vetor";
+            this.label_Vetor.Size = new System.Drawing.Size(32, 13);
+            this.label_Vetor.TabIndex = 5;
+            this.label_Vetor.Text = "Vetor";
+            this.label_Vetor.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(511, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Posição do Usuário no Vetor";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(693, 517);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_Vetor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_cadastrar);
+            this.Controls.Add(this.bt_logar);
+            this.Controls.Add(this.caixa_senha);
+            this.Controls.Add(this.caixa_nome);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox caixa_nome;
+        private System.Windows.Forms.MaskedTextBox caixa_senha;
+        private System.Windows.Forms.Button bt_logar;
+        private System.Windows.Forms.Button bt_cadastrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_Vetor;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RedeSocial/Tela_Perfil_Usuário.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace RedeSocial
+{
+    partial class Tela_Perfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Label_Nome = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Label_Id = new System.Windows.Forms.Label();
+            this.Label_Data = new System.Windows.Forms.Label();
+            this.Label_email = new System.Windows.Forms.Label();
+            this.Label_name = new System.Windows.Forms.Label();
+            this.foto_perfil = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.foto_perfil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seguir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Label_Nome
+            // 
+            this.Label_Nome.AutoSize = true;
+            this.Label_Nome.Location = new System.Drawing.Point(44, 57);
+            this.Label_Nome.Name = "Label_Nome";
+            this.Label_Nome.Size = new System.Drawing.Size(106, 13);
+            this.Label_Nome.TabIndex = 1;
+            this.Label_Nome.Text = "Nome Usuário Amigo";
+            this.Label_Nome.Click += new System.EventHandler(this.Label_Nome_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data de Nascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(118, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "E-Mail:";
+            // 
+            // Label_Id
+            // 
+            this.Label_Id.AutoSize = true;
+            this.Label_Id.Location = new System.Drawing.Point(207, 178);
+            this.Label_Id.Name = "Label_Id";
+            this.Label_Id.Size = new System.Drawing.Size(18, 13);
+            this.Label_Id.TabIndex = 6;
+            this.Label_Id.Text = "ID";
+            this.Label_Id.Click += new System.EventHandler(this.Label_Id_Click);
+            // 
+            // Label_Data
+            // 
+            this.Label_Data.AutoSize = true;
+            this.Label_Data.Location = new System.Drawing.Point(207, 282);
+            this.Label_Data.Name = "Label_Data";
+            this.Label_Data.Size = new System.Drawing.Size(30, 13);
+            this.Label_Data.TabIndex = 7;
+            this.Label_Data.Text = "Data";
+            // 
+            // Label_email
+            // 
+            this.Label_email.AutoSize = true;
+            this.Label_email.Location = new System.Drawing.Point(207, 247);
+            this.Label_email.Name = "Label_email";
+            this.Label_email.Size = new System.Drawing.Size(43, 13);
+            this.Label_email.TabIndex = 8;
+            this.Label_email.Text = "Usuário";
+            // 
+            // Label_name
+            // 
+            this.Label_name.AutoSize = true;
+            this.Label_name.Location = new System.Drawing.Point(207, 210);
+            this.Label_name.Name = "Label_name";
+            this.Label_name.Size = new System.Drawing.Size(43, 13);
+            this.Label_name.TabIndex = 9;
+            this.Label_name.Text = "Usuário";
+            // 
+            // foto_perfil
+            // 
+            this.foto_perfil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.foto_perfil.Location = new System.Drawing.Point(320, 57);
+            this.foto_perfil.Name = "foto_perfil";
+            this.foto_perfil.Size = new System.Drawing.Size(558, 430);
+            this.foto_perfil.TabIndex = 10;
+            this.foto_perfil.TabStop = false;
+            this.foto_perfil.Click += new System.EventHandler(this.foto_perfil_Click);
+            // 
+            // Tela_Perfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 599);
+            this.Controls.Add(this.foto_perfil);
+            this.Controls.Add(this.Label_name);
+            this.Controls.Add(this.Label_email);
+            this.Controls.Add(this.Label_Data);
+            this.Controls.Add(this.Label_Id);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Label_Nome);
+            this.Controls.Add(this.button1);
+            this.Name = "Tela_Perfil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela Perfil";
+            this.Load += new System.EventHandler(this.Tela_Perfil_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.foto_perfil)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Label_Nome;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Label_Id;
+        private System.Windows.Forms.Label Label_Data;
+        private System.Windows.Forms.Label Label_email;
+        private System.Windows.Forms.Label Label_name;
+        private System.Windows.Forms.PictureBox foto_perfil;
+    }
+}
